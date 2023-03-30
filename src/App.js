8f6dd188d7c2de09ed80936b9433789792c81a5a
nt <42067723+phi7@users.noreply.github.com>
2023-03-30 15:32:54 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network } from "alchemy-sdk";
+import { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
@@ -10,7 +10,6 @@ const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
-
 
 // In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
@@ -21,16 +20,43 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockDifficulty, setBlockDifficulty] = useState();
+  const [blockGasLimit, setBlockGasLimit] = useState();
+  const [blockTransactions, setBlockTransactions] = useState();
 
   useEffect(() => {
     async function getBlockNumber() {
       setBlockNumber(await alchemy.core.getBlockNumber());
     }
+    async function getBlockDifficulty() {
+      setBlockDifficulty((await alchemy.core.getBlock()).difficulty);
+    }
+    //getBlockGasLimit()という関数を作る
+    async function getBlockGasLimit() {
+      setBlockGasLimit((await alchemy.core.getBlock()).gasLimit.toString());
+    }
+
+    //getBlockTransactions();を書く
+    async function getBlockTransactions() {
+      setBlockTransactions((await alchemy.core.getBlock()).transactions);
+    }
 
     getBlockNumber();
+    getBlockDifficulty();
+    getBlockGasLimit();
+    // getBlockTransactions();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  return (
+    <div className="App">
+      <div>Block Number: {blockNumber}</div>
+      <div>Block Difficulty: {blockDifficulty}</div>
+      <div>Block Gas Limit: {blockGasLimit}</div>
+      <div>Block Transactions: </div>
+      {/* //blockTransactions内のtransactionを表示する．ただし最初の10個だけ */}
+      {/* さらにそのトランザクションをクリックするとそのトランザクションの詳細情報が表示されるページへ遷移する */}
+    </div>
+  );
 }
 
 export default App;
